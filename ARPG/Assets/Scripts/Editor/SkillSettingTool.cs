e458b86aa05e8c04331ba53df7b31ec76d9d6038
yong-gyun <sygttgudthfr@gmail.com>
2025-06-04 19:13:37 +0900

    스킬 툴 작업

--- ARPG/Assets/Scripts/Editor/SkillSettingTool.cs
+++ ARPG/Assets/Scripts/Editor/SkillSettingTool.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,9 +7,11 @@ using UnityEngine;
 public class SkillSettingTool : EditorWindow
 {
     private const float SPACE_INTERVAL = 2.5f;
-    private const float FRAME_RATE = 60f;
-    private float _playFrame = 0f;
-    private float _playSpeed = 1f;
+    private float _playTime = 0f;       //시간 단위로 봄
+    private float _playSpeed = 1f;      //배속
+    private float _fieldOfView = 30f;
+
+    private bool _isDrag = false;
     private bool _hasFocus = false;
     private bool _isPlaying = false;
 
@@ -19,11 +22,19 @@ public class SkillSettingTool : EditorWindow
     private AnimationClip _animClip;
     private SkillSettingData _skillSettingData;
 
+    private const float EDITOR_WIDTH = 1200f;
+    private const float EDITOR_HEIGHT = 800f;
+
+    private const float PREVIEW_WIDTH = 900f;
+    private const float PREVIEW_HEIGHT = 500f;
+
+    private List<Renderer> _rendererBuffer = new List<Renderer>();
+
     [MenuItem("Tools/스킬 세팅 툴")]
     static void OpenWindow()
     {
         var window = GetWindow<SkillSettingTool>("스킬 세팅 툴");
-        window.minSize = new Vector2(1200, 500);
+        window.minSize = new Vector2(EDITOR_WIDTH, EDITOR_HEIGHT);
     }
 
     private void OnFocus()
@@ -56,50 +67,74 @@ public class SkillSettingTool : EditorWindow
             {
                 EditorGUILayout.BeginVertical("box", GUILayout.Width(900), GUILayout.Height(500));
                 {
-                    Texture result = null;
-
-                    if (_previewObject != null)
-                    {
-                        Rect previewRect = new Rect();
-                        previewRect.width = 900;
-                        previewRect.height = 475;
-
-                        _previewUtility.BeginPreview(previewRect, GUIStyle.none);
-
-                        _previewUtility.camera.transform.LookAt(_previewObject.transform);
-                        _previewUtility.camera.fieldOfView = 30f;
-
-                        _previewUtility.Render();
-                        result = _previewUtility.EndPreview();
-                    }
-
-                    GUILayout.Box(result, GUILayout.Width(900), GUILayout.Height(475));
-                    EditorGUILayout.BeginHorizontal();
-                    {
-                        if (GUILayout.Button(_isPlaying == true ? "II" : "▷", GUILayout.Width(25)) == true)
-                        {
-                            
-                        }
-
-                        GUILayout.Label($"{_playFrame:F2}", GUILayout.Width(30));
-
-                        _playFrame = GUILayout.HorizontalSlider(_playFrame, 0f, _animClip != null ? _animClip.length : 1f, GUILayout.Width(690));
-                        GUILayout.Space(SPACE_INTERVAL);
-
-                        _playSpeed = GUILayout.HorizontalSlider(_playSpeed, 0.1f, 2f, GUILayout.Width(100));
-                        GUILayout.Label($"{_playSpeed:F2}x", GUILayout.Width(40));
-                    }
-                    EditorGUILayout.EndHorizontal();
-
+                    DrawPreviewer(PREVIEW_WIDTH, PREVIEW_HEIGHT);
+                    DrawEvents();
                 }
                 EditorGUILayout.EndVertical();
             }
             EditorGUILayout.EndHorizontal();
 
-
             ShowInputDatas();
         }
         EditorGUILayout.EndVertical();
+    }
+
+    private void DrawPreviewer(float width, float height)
+    {
+        Texture texture = null;
+        if (_previewObject != null)
+        {
+            Rect previewRect = new Rect();
+            previewRect.width = width;
+            previewRect.height = height;
+
+            _previewUtility.BeginPreview(previewRect, GUIStyle.none);
+            _previewUtility.camera.fieldOfView = _fieldOfView;
+
+            Bounds bounds = new Bounds(_previewObject.transform.position, Vector3.zero);
+            foreach (Renderer renderer in _rendererBuffer)
+                bounds.Encapsulate(renderer.bounds);
+
+            Vector3 center = bounds.center;
+            float size = bounds.size.magnitude;
+            float distance = size / (2.0f * Mathf.Tan(0.5f * _fieldOfView * Mathf.Deg2Rad));
+
+            Camera previewCam = _previewUtility.camera;
+            previewCam.transform.position = center - Vector3.forward * distance;
+            previewCam.transform.LookAt(_previewObject.transform);
+
+            if (_animClip != null)
+                _animClip.SampleAnimation(_previewObject, _playTime);
+
+            _previewUtility.Render();
+            Texture result = _previewUtility.EndPreview();
+        }
+
+        GUILayout.Box(texture, GUILayout.Width(900), GUILayout.Height(475));
+    }
+
+    private void DrawEvents()
+    {
+        EditorGUILayout.BeginHorizontal(GUILayout.Width(PREVIEW_WIDTH));
+        {
+            if (GUILayout.Button(_isPlaying == true ? "II" : "▷", GUILayout.Width(25)) == true)
+            {
+                _isPlaying = !_isPlaying;
+            }
+
+            _playTime = EditorGUILayout.FloatField(_playTime, GUILayout.Width(30));
+            _playTime = GUILayout.HorizontalSlider(_playTime, 0f, _animClip != null ? _animClip.length : 1f, GUILayout.Width(690));
+            GUILayout.Space(SPACE_INTERVAL);
+
+            _playSpeed = GUILayout.HorizontalSlider(_playSpeed, 0.1f, 2f, GUILayout.Width(50));
+            GUILayout.Label($"{_playSpeed:F2}x", GUILayout.Width(40));
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void AddAnimationEvent()
+    {
+
     }
 
     private void ShowInputDatas()
@@ -120,6 +155,10 @@ public class SkillSettingTool : EditorWindow
                 _previewObject = GameObject.Instantiate(_prefab);
                 _previewUtility.camera.transform.position = new Vector3(0, 0, -10);
                 _previewUtility.AddSingleGO(_previewObject);
+
+                var renderers = _previewObject.GetComponentsInChildren<Renderer>();
+                foreach (Renderer renderer in renderers)
+                    _rendererBuffer.Add(renderer);
             }
         }
         EditorGUILayout.EndVertical();
